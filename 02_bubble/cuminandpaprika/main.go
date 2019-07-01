3c27f4c0601821e748197dce502a5d9cfb7b7ac0
Jack Zheng <jack.zheng@anz.com>
2019-07-02 08:40:12 +1000

    Add tests and function for bubble sort

---
+++ 02_bubble/cuminandpaprika/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(bubble([]int{3, 2, 1, 5}))
+}
+
+func bubble(s []int) []int {
+	return s
+}
---
+++ 02_bubble/cuminandpaprika/main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bytes"
+	"strconv"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMainCallsBubble(t *testing.T) {
+	// Arrange
+	var buf bytes.Buffer
+
+	main()
+
+	// Assert
+	expected := strconv.Quote("")
+	actual := strconv.Quote(buf.String())
+
+	if expected != actual {
+		t.Errorf("Unexpected behavior for main")
+		t.Errorf("expected: %s\nactual:%s", expected, actual)
+	}
+}
+
+func TestBubbleSortsSingle(t *testing.T) {
+	a := assert.New(t)
+
+	input := []int{1}
+	expected := []int{1}
+	actual := bubble(input)
+
+	a.Equal(expected, actual, "Bubble Sorts Single Input")
+	// Check it's a copy
+}
+
+func TestBubbleSortReturnsSorted(t *testing.T) {
+	a := assert.New(t)
+
+	testCases := map[string]struct {
+		input []int
+		want  []int
+	}{
+		"single":     {input: []int{1}, want: []int{1}},
+		"duplicates": {input: []int{2, 1, 4, 1}, want: []int{1, 1, 2, 4}},
+		"negative":   {input: []int{-1, 1, -4, 0}, want: []int{-4, -1, 0, 1}},
+	}
+
+	for name, test := range testCases {
+		// t.Run creates a sub test and runs it like a normal test
+		t.Run(name, func(t *testing.T) {
+			result := bubble(test.input)
+			a.Equal(result, test.want, "bubble Failed to Sort")
+		})
+	}
+}
