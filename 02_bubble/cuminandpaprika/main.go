7564d06ea0c4baae080699b8d788a074c6609269
Jack Zheng <jack.zheng@anz.com>
2019-08-06 22:34:17 +1000

    add working code

--- 02_bubble/cuminandpaprika/main.go
+++ 02_bubble/cuminandpaprika/main.go
@@ -13,12 +13,30 @@ func main() {
 }
 
 func bubble(unsorted []int) []int {
-	for index := 0; index < len(unsorted) -1; index++ {
-		fmt.Fprintf(out, "Comparing %v and %v\n", unsorted[index],unsorted[index+1])
-		if (unsorted[index] > unsorted[index+1]) {
-			unsorted[index+1], unsorted[index] = unsorted[index], unsorted[index+1]
-			fmt.Fprintf(out, "Swapping %v and %v\n", unsorted[index],unsorted[index+1])
+	var swapped bool
+	for {
+		for index := 0; index < len(unsorted) -1; index++ {
+			fmt.Fprintf(out, "Comparing %v and %v\n", unsorted[index],unsorted[index+1])
+			fmt.Fprintln(out, unsorted[index:index+2])
+			swapped = sort(unsorted[index:index+2])
+		}
+		if (!swapped) {
+			return unsorted
 		}
 	}
+
 	return unsorted
+}
+
+func sort(unsortedPair []int) bool {
+	if (leftIsGreater(unsortedPair)) {
+		unsortedPair[0], unsortedPair[1] = unsortedPair[1], unsortedPair[0]
+		fmt.Fprintf(out, "Swapped %v and %v\n", unsortedPair[0],unsortedPair[1])
+		return true
+	}
+	return false
+} 
+
+func leftIsGreater(pair []int) bool {
+	return (pair[0] > pair[1])
 }
